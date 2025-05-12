eb21490694fd4f4fd9ccdef870c8315ffbdc1853
cunovske-jazera-bez-odpadkov <62881096+cunovske-jazera-bez-odpadkov@users.noreply.github.com>
2025-05-12 09:13:30 +0200

    Add files via upload

---
+++ GJSON/geojsonLesnaCestaPriHugov.js
@@ -0,0 +1,36 @@
+var geojsonLesnaCestaPriHugov = {
+  "type": "Feature",
+  "properties":{
+    "name"  : "LesnaCestaPriHugov"
+  },
+  "geometry":{
+    "type" : "LineString",
+    "coordinates" : [
+          [
+            17.16602626747207,
+            48.04604891831523,
+            0
+          ],
+          [
+            17.17012858625068,
+            48.04612223182665,
+            0
+          ],
+          [
+            17.17864502031111,
+            48.04628452558745,
+            0
+          ],
+          [
+            17.1809173038666,
+            48.04538280497474,
+            0
+          ]
+        ]
+  }
+};
+var	geojsonStyleLesnaCestaPriHugov = {
+	"color" : "#00ff00",
+	"weight" : 5,
+	"opacity" : 0.90
+};	
---
+++ GJSON/geojsonLesnaCestaVedlaFarskej.js
@@ -0,0 +1,36 @@
+var geojsonLesnaCestaVedlaFarskej = {
+  "type": "Feature",
+  "properties":{
+    "name"  : "LesnaCestaVedlaFarskej"
+  },
+  "geometry":{
+    "type" : "LineString",
+    "coordinates" : [
+          [
+            17.17433675527185,
+            48.04190463003367,
+            0
+          ],
+          [
+            17.17510971580643,
+            48.04260339619098,
+            0
+          ],
+          [
+            17.17607970374612,
+            48.04345156041072,
+            0
+          ],
+          [
+            17.17791248438041,
+            48.04479723768052,
+            0
+          ]
+        ]
+  }
+};
+var	geojsonStyleLesnaCestaVedlaFarskej = {
+	"color" : "#00ff00",
+	"weight" : 5,
+	"opacity" : 0.90
+};	

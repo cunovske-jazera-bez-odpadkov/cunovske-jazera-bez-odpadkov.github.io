b4e6b8cc0416cfbd6889eb7b2237bd71d5345411
cunovske-jazera-bez-odpadkov <62881096+cunovske-jazera-bez-odpadkov@users.noreply.github.com>
2025-05-12 09:17:50 +0200

    Add files via upload

---
+++ JSZDROJE/AkoSaDostat.js
@@ -0,0 +1,52 @@
+var AkoSaDostatStr, AkoSaDostatNadpisStr;
+AkoSaDostatNadpisStr='Ako sa dostanete na jazerá';
+AkoSaDostatStr='Na Čunovské jazerá môžete prísť z viacerých smerov, <br/> \
+viacerými cestami a rôznymi dopravnými prostriedkami. Priamo k jazerám je vhodné prísť peši, poprípade \
+na koni. Otázny je pohyb na bicykli alebo kolobežke, keďže sa nachádzate minimálne v 2. stupni ochrany prírody \
+kde podľa § 13 (1) b) <br/><br/><i>...je zakázané jazdiť na bicykli, trojkolke, kolobežke alebo samovyvažovacom vozidle na pozemkoch za hranicami zastavaného územia obce mimo diaľnice, cesty, miestnej komunikácie, účelovej komunikácie45) a vyznačenej cyklotrasy.</i>\
+<br/><br/>Pomedzi jazerá prechádza asfaltová cesta, tiahnuca sa od hlavnej cesty a \
+končiaca pri areáli vodného zdroja, cez ktorý sa ale ďalej nedostanete.\
+<br/><br/> \
+Ľudia sa na jazerá dostávajú najčastejšie buď pešo, na bicykli či kolobežke, niektorí možno na koni.\
+Pokiaľ nemáte povolenie či kľúče od rampy, autom sa sem nedostanete. \
+Ďalej od jazier sa však už môžete dostať autom, MHD či vlakom a k jazerám préjsť peši. \
+<br/><br/> \
+<span class="atributyPodNadpisu">\
+Príchod autom a iným motorovým vozidlom</span><br/>\
+Autom sa na asfaltovú odbočku k jazerám dostanete až po rampu,\
+ ktorá je umiestnená za zastavanou časťou Rusoviec. \
+Môžete tu však parkovať iba s povolením, ináč riskujete pokutu. \
+Ak nemáte povolenie, musíte hľadať parkovanie niekde ďalej. Napr. zaparkovať pri hlavnej ceste a následne sa peši presunúť k jazerám. \
+Parkovanie pri hlavnej ceste je však dosť komplikované a neodporúča sa. \
+Obmedzovanie parkovania je spojené s ochranou jazier proti masovej návštevnosti.\
+<br/><br/> \
+<span class="atributyPodNadpisu">\
+Príchod MHD</span><br/>\
+Autobusová zastávka s názvom Čunovské jazerá je umiestnená na hlavnej ceste blízko pri odbočke na jazerá. \
+Po vystúpení z autobusu musíte najprv nájsť blízku asfaltovú odbočku, \
+a popri novostavbách umiestnených na ľavej strane cesty pôjdete stále rovno.\
+Najprv prídete k rampe s mini parkoviskom a potom pokračujete lesnou cestou ďalej.\
+Asfalka vás prevedie cez Rusovské rameno a zakrátko sa ocitnete \
+na Čunovských jazerách.\
+<br/><br/> \
+<span class="atributyPodNadpisu">\
+Príchod na jazerá pešo</span><br/>\
+Najlepšie urobíte ak sa na jazerá vyberiete pešo. \
+Okrem asfaltky, k jazerám vedú aj rôzne lesné cesty a cestičky. Ľudia najčastejšie \
+využívajú dlhú lesnú cestu od Rusoviec, alebo z Čunova. \
+Na tieto cesty sa dá napojiť buď z hrádze alebo samotných obcí na viacerých miestach. K jazerám smerujú aj turistické cesty. \
+Čunovské jazerá sú považované za prírodnú atrakciu Bratislavy a dajú sa k nim urobiť krásne túry plné úchvatných scenérií.\
+<br/><br/>\
+<span class="atributyPodNadpisu">\
+Príchod na jazerá na bicykli</span><br/>\
+Lesné cestičky smerujúce k jazerám využívajú aj cyklisti, avšak pohyb na bicykli v lesoch v 2. pásme ochrany je otázny. \
+Veľa cyklistov prichádza na jazerá najprv po hrádzi vodného diela a potom sa napojí na jednu z lesných ciest. \
+Je to ekologický a zdravý spôsob dopravy na jazerá. Po lesných cestách sa však treba pohybovať pomaly a ohladuplne voči okoliu.\
+<br/><br/> \
+<span class="atributyPodNadpisu">\
+Doprava vlakom</span><br/>\
+Do Rusoviec sa môžete dostať aj vlakom, ktorý však chodí iba občas. \
+Je to veľmi rýchla doprava a môžete ju kombinovať s MHD a dopraviť sa až na MHD zastávku "Čunovské jazerá". \
+Na vlak vrámci Bratislavy smiete využiť aj tzv."električenku" poprípade IDS BK aplikáciu. Avšak pozor! MHD sms lístky tu neplatia. \
+Po príchode vlakom do Rusoviec, si k jazerám môžete urobiť aj túru. Musíte sa však dostať Gaštanovou alejou popri domoch do lesa a odtiaľ k jazerám už vedie lesná cesta. \
+Je to síce dlhšia trasa, ale stojí za tu námahu.';
